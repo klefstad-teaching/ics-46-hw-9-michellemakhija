dc6ea91ef7b16d6c86e4bd258a269ba931b3e5de
Michelle Makhija <makhijam@uci.edu>
2025-03-14 23:36:01 +0000

    wrote the print, load and is adjacent

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -29,12 +29,36 @@ bool edit_distance_within(const std::string& str1, const std::string& str2, int 
     edits += abs(len1 - i - (len2 - j));
     return edits <= d;
 }
-bool is_adjacent(const string& word1, const string& word2);
+bool is_adjacent(const string& word1, const string& word2) {
+    return edit_distance_within(word1, word2, 1);
+}
 vector<string> generate_word_ladder(const string& begin_word, const string& end_word, const set<string>& word_list) {
     
 }
-void load_words(set<string> & word_list, const string& file_name);
-void print_word_ladder(const vector<string>& ladder);
+void load_words(set<string> & word_list, const string& file_name) {
+    ifstream file(in);
+    if (!in) {
+        cout << "file not loaded :(" << endl;
+        return;
+    }
+    string word;
+    while (in >> word) {
+        for (char& c: word) {
+            c = tolower(c);
+        }
+        word_list.insert(word);
+    }
+    in.close();
+}
+void print_word_ladder(const vector<string>& ladder) {
+    if (ladder.empty()) {
+        cout << "empty ladder" << endl;
+        return;
+    }
+    for (size_t i = 0; i < ladder.size(); ++i) {
+        cout << ladder[i] << " ";
+    }
+}
 void verify_word_ladder() {
     set<string> word_list;
     load_words(word_list, "words.txt");
