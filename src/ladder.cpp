635a49f7a2caa934ea3f75fde5c53418a8f8a1da
Michelle Makhija <makhijam@uci.edu>
2025-03-14 23:22:55 +0000

    wrote edit dist within, untested

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -4,7 +4,31 @@ using namespace std;
 void error(string word1, string word2, string msg) {
     cout << word1 << msg << word2 << endl;
 }
-bool edit_distance_within(const std::string& str1, const std::string& str2, int d);
+bool edit_distance_within(const std::string& str1, const std::string& str2, int d) {
+    int stringLength1 = str1.size();
+    int stringLength2 = str2.size();
+    if (abs(stringLength1-stringLength2) > d) { return false; }
+    
+    int editsToWord = 0;
+    int i, j;
+
+    while (i < stringLength1 && j < stringLength2) {
+        if (str1[i] != str2[j]) {
+            edits++;
+            if (edits > d) {return false;}
+            if (len1 > len2) i++;
+            else if (len1 < len2) j++;
+            else { i++; j++; }
+        }
+        else {
+            i++;
+            j++;
+        }
+    }
+
+    edits += abs(len1 - i - (len2 - j));
+    return edits <= d;
+}
 bool is_adjacent(const string& word1, const string& word2);
 vector<string> generate_word_ladder(const string& begin_word, const string& end_word, const set<string>& word_list) {
     
