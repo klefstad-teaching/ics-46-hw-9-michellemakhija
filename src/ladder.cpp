6c622df4bab20af4baf0a967fff7b05134489007
Michelle Makhija <makhijam@uci.edu>
2025-03-14 22:59:21 +0000

    added the verify and error

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -1,9 +1,23 @@
 #include "ladder.h"
+using namespace std;
 
-void error(string word1, string word2, string msg);
+void error(string word1, string word2, string msg) {
+    cout << word1 << msg << word2 << endl;
+}
 bool edit_distance_within(const std::string& str1, const std::string& str2, int d);
 bool is_adjacent(const string& word1, const string& word2);
-vector<string> generate_word_ladder(const string& begin_word, const string& end_word, const set<string>& word_list);
+vector<string> generate_word_ladder(const string& begin_word, const string& end_word, const set<string>& word_list) {
+    
+}
 void load_words(set<string> & word_list, const string& file_name);
 void print_word_ladder(const vector<string>& ladder);
-void verify_word_ladder();
+void verify_word_ladder() {
+    set<string> word_list;
+    load_words(word_list, "words.txt");
+    my_assert(generate_word_ladder("cat", "dog", word_list).size() == 4);
+    my_assert(generate_word_ladder("marty", "curls", word_list).size() == 6);
+    my_assert(generate_word_ladder("code", "data", word_list).size() == 6);
+    my_assert(generate_word_ladder("work", "play", word_list).size() == 6);
+    my_assert(generate_word_ladder("sleep", "awake", word_list).size() == 8);
+    my_assert(generate_word_ladder("car", "cheat", word_list).size() == 4);
+}
---
+++ src/ladder_main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include "ladder.cpp"
+
+int main() {
+    verify_word_ladder();
+}
