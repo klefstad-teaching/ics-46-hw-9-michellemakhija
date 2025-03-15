a0454ab0894773dcd3dea5755426b5052f3100b7
Michelle Makhija <makhijam@uci.edu>
2025-03-15 00:36:52 +0000

    fixed printing

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -80,12 +80,14 @@ void load_words(set<string> & word_list, const string& file_name) {
 }
 void print_word_ladder(const vector<string>& ladder) {
     if (ladder.empty()) {
-        cout << "empty ladder" << endl;
+        cout << "No word ladder found." << endl;
         return;
     }
+    cout << "Word ladder found: ";
     for (size_t i = 0; i < ladder.size(); ++i) {
         cout << ladder[i] << " ";
     }
+    cout << endl;
 }
 void verify_word_ladder() {
     set<string> word_list;
